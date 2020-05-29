3e0c5a5a43a1f8ac246730386cb36de029ed9dfa
Harshit Verma <harshitverma26@gmail.com>
2020-05-29 18:19:57 +0530

    shopping_cart

--- src/components/reducers/cartReducer.js
+++ src/components/reducers/cartReducer.js
@@ -9,12 +9,12 @@ import { ADD_TO_CART,REMOVE_ITEM,SUB_QUANTITY,ADD_QUANTITY,ADD_SHIPPING } from '
 
 const initState = {
     items: [
-        {id:1,title:'Winter body', desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, ex.", price:880,img:Item1},
-        {id:2,title:'Adidas', desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, ex.", price:960,img: Item2},
-        {id:3,title:'Vans', desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, ex.",price:770,img: Item3},
-        {id:4,title:'White', desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, ex.", price:543,img:Item4},
-        {id:5,title:'Cropped-sho', desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, ex.", price:1600,img: Item5},
-        {id:6,title:'Blues', desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, ex.",price:900,img: Item6}
+        {id:1,title:'Winter body', desc: "ASIAN Shoes Wonder-13 brown, ex.", price:880,img:Item1},
+        {id:2,title:'Adidas', desc: "Adidas Shoes black, ex.", price:960,img: Item2},
+        {id:3,title:'Fila', desc: "Fila Shoes with checkstyle, ex.",price:770,img: Item3},
+        {id:4,title:'White', desc: "Sport stylist White shoes, ex.", price:543,img:Item4},
+        {id:5,title:'Cropped-sho', desc: "Long Boot shoes, ex.", price:1600,img: Item5},
+        {id:6,title:'Campus', desc: "Campus casual shoe, ex.",price:900,img: Item6}
     ],
     addedItems:[],
     total: 0
